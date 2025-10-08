33f0448e33a67a91165ae4df4ef107c4beac05cd
Giorno <DoctorRon64@users.noreply.github.com>
2025-10-08 15:33:00 +0200

    Trying to handle the ship placing alghoritmn

--- batalla_naval/batalla_naval.cpp
+++ batalla_naval/batalla_naval.cpp
@@ -12,16 +12,55 @@ int main() {
 
     char boardPlayer1[10][10];
     char boardPlayer2[10][10];
-   
-    //========= Initialize boards ======
+    char visibleBoardPlayer1[10][10];
+    char visibleBoardPlayer2[10][10];
+
+    //-----========= Initialize boards ======-----
     for (int i = 0; i < 10; ++i) {
         for (int j = 0; j < 10; ++j) {
             boardPlayer1[i][j] = '~';
             boardPlayer2[i][j] = '~';
+            visibleBoardPlayer1[i][j] = '~';
+            visibleBoardPlayer2[i][j] = '~';
         }
     }
 
-    //===== Display boards ========
+    //------========== Place Ships ========-------
+    int shipSizes[] = { 3, 4, 5, 6 };
+    char shipSymbols[] = { '3', '4', '5', '6' }; //because I cant use cast because the assingment is so basic :(
+
+    for (int i = 0; i < 4; i++) {
+        int currentShipSize = shipSizes[i];
+        bool placed = false;
+        int attempts = 0;
+
+        //Based on that orientation and the ship length, choose a random starting row and column such that the ship will fit inside the 10×10 grid.
+        //Check every cell along the intended ship path to ensure none are already occupied.
+        //If all cells are free, mark those cells as containing the ship and stop trying for this ship.
+        //If any cell is occupied, discard this attempt and try a new random position / orientation.
+
+        while (!placed) {
+            //int dir = std::rand() % 2; //randomized vertical or horizontal;
+            int dir = 0;
+
+            if (dir == 0) {
+                int randomRow = std::rand() % (10 - currentShipSize);
+                int randomColomn = std::rand() % (10 - currentShipSize);
+
+                for (int j = 0; j < currentShipSize; j++) {
+                    boardPlayer1[randomRow][randomColomn] = shipSymbols[i];
+                    randomColomn++;
+                }
+
+                placed = true;
+            } else {
+                int randomPos = std::rand() % 10;
+
+            }
+        }
+    }
+
+    //----===== Display boards ========----
     std::cout << "Jugador 1: " << std::endl;
     std::cout << "==========" << std::endl;
     for (int i = 0; i < 10; ++i) {
@@ -32,7 +71,6 @@ int main() {
     }
 
     std::cout << std::endl;
-
     std::cout << "Jugador 2: " << std::endl;
     std::cout << "==========" << std::endl;
     for (int i = 0; i < 10; ++i) {
@@ -41,21 +79,6 @@ int main() {
         }
         std::cout << std::endl;
     }
-
-    int shipSizes[] = { 3, 4, 5, 6 };
-    std::srand(std::time(NULL));
-
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; ++j) {
-            if (boardPlayer1[i][j] == '~') {
-                //if (ship == 0) return;
-                //ship--;
-                //board[i][j] = shipChar;
-            }
-        }
-    }
-
-
 
     return 0;
 }
