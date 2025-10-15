a5893116ac7ec87f30989f427e9749f311f46a1a
Giorno <DoctorRon64@users.noreply.github.com>
2025-10-15 11:56:24 +0200

    Cleaning Code

--- batalla_naval/batalla_naval.cpp
+++ batalla_naval/batalla_naval.cpp
@@ -4,250 +4,272 @@
 #include <random>
 
 int main() {
-    std::cout << "Bienvenido a Batalla naval!" << std::endl;
-    std::cout << "================================" << std::endl;
-    std::cout << std::endl;
+	std::cout << "Bienvenido a Batalla naval!" << std::endl;
+	std::cout << "================================" << std::endl;
+	std::cout << std::endl;
 
-    const int BOARD_SIZE = 10;
-    char boardPlayer1[BOARD_SIZE][BOARD_SIZE];
-    char boardPlayer2[BOARD_SIZE][BOARD_SIZE];
-    char visibleBoardPlayer1[BOARD_SIZE][BOARD_SIZE];
-    char visibleBoardPlayer2[BOARD_SIZE][BOARD_SIZE];
+	const int BOARD_SIZE = 10;
+	const int SHIP_COUNT = 4;
+	const char WATER = '~';
+	const char HIT_MARK = 'X';
+	const char MISS_MARK = 'O';
 
-    //-----========= Initialize boards ======-----
-    for (int i = 0; i < 10; ++i) {
-        for (int j = 0; j < 10; ++j) {
-            boardPlayer1[i][j] = '~';
-            boardPlayer2[i][j] = '~';
-            visibleBoardPlayer1[i][j] = '~';
-            visibleBoardPlayer2[i][j] = '~';
-        }
-    }
+	char boardPlayer1[BOARD_SIZE][BOARD_SIZE];
+	char boardPlayer2[BOARD_SIZE][BOARD_SIZE];
+	char visibleBoardPlayer1[BOARD_SIZE][BOARD_SIZE];
+	char visibleBoardPlayer2[BOARD_SIZE][BOARD_SIZE];
 
-    //------========== Place Ships ========-------
-    std::srand(std::time(0));
-    int shipSizes[] = { 3, 4, 5, 6 };
-    char shipSymbols[] = { '3', '4', '5', '6' }; //because I cant use cast because the assingment is so basic :(
+	//-----========= Initialize boards ======-----
+	for (int i = 0; i < 10; ++i) {
+		for (int j = 0; j < 10; ++j) {
+			boardPlayer1[i][j] = WATER;
+			boardPlayer2[i][j] = WATER;
+			visibleBoardPlayer1[i][j] = WATER;
+			visibleBoardPlayer2[i][j] = WATER;
+		}
+	}
 
-    //Placing ships for Player 1
-    for (int i = 0; i < 4; i++) {
-        int currentShipSize = shipSizes[i];
-        bool placed = false;
-        int attempts = 0;
+	//------========== Place Ships ========-------
+	std::srand(std::time(0));
+	int shipSizes[] = { 3, 4, 5, 6 };
+	char shipSymbols[] = { '3', '4', '5', '6' }; //because I cant use cast because the assingment is so basic :(
 
-        while (!placed && attempts < 9999) {
-            attempts++;
+	//Placing ships for Player 1
+	for (int i = 0; i < 4; i++) {
+		int currentShipSize = shipSizes[i];
+		bool placed = false;
+		int attempts = 0;
 
-            //Check for Horizontal or Vertical side first
-            int dir = std::rand() % 2;
-            int row, col = 0;
-            if (dir == 0) {
-                row = std::rand() % BOARD_SIZE;
-                col = std::rand() % (BOARD_SIZE - currentShipSize + 1);
-            }
-            else {
-                row = std::rand() % (BOARD_SIZE - currentShipSize + 1);
-                col = std::rand() % BOARD_SIZE;
-            }
-            
-            //Check if is able to place sentire boat 
-            bool canPlace = true;
-            for (int j = 0; j < currentShipSize; j++) {
-                int r = (dir == 1) ? row + j : row;
-                int c = (dir == 1) ? col : col + j;
+		while (!placed && attempts < 9999) {
+			attempts++;
 
-                if (boardPlayer1[r][c] != '~') {
-                    canPlace = false;
-                    break;
-                }
-            }
+			//Check for Horizontal or Vertical side first
+			int dir = std::rand() % 2;
+			int row, col = 0;
+			if (dir == 0) {
+				row = std::rand() % BOARD_SIZE;
+				col = std::rand() % (BOARD_SIZE - currentShipSize + 1);
+			}
+			else {
+				row = std::rand() % (BOARD_SIZE - currentShipSize + 1);
+				col = std::rand() % BOARD_SIZE;
+			}
 
-            //if all are free place it:
-            if (canPlace) {
-                for (int j = 0; j < currentShipSize; j++) {
-                    int r = (dir == 1) ? row + j : row;
-                    int c = (dir == 1) ? col : col + j;
-                    boardPlayer1[r][c] = shipSymbols[i];
-                }
-                placed = true;
-            }
-        }
-    }
+			//Check if is able to place sentire boat 
+			bool canPlace = true;
+			for (int j = 0; j < currentShipSize; j++) {
+				int r = (dir == 1) ? row + j : row;
+				int c = (dir == 1) ? col : col + j;
 
-    //placplayer 2
-    for (int i = 0; i < 4; i++) {
-        int currentShipSize = shipSizes[i];
-        bool placed = false;
-        int attempts = 0;
+				if (boardPlayer1[r][c] != WATER) {
+					canPlace = false;
+					break;
+				}
+			}
 
-        while (!placed && attempts < 9999) {
-            attempts++;
+			//if all are free place it:
+			if (canPlace) {
+				for (int j = 0; j < currentShipSize; j++) {
+					int r = (dir == 1) ? row + j : row;
+					int c = (dir == 1) ? col : col + j;
+					boardPlayer1[r][c] = shipSymbols[i];
+				}
+				placed = true;
+			}
+		}
+	}
 
-            //Check for Horizontal or Vertical side first
-            int dir = std::rand() % 2;
-            int row, col = 0;
-            if (dir == 0) {
-                row = std::rand() % BOARD_SIZE;
-                col = std::rand() % (BOARD_SIZE - currentShipSize + 1);
-            }
-            else {
-                row = std::rand() % (BOARD_SIZE - currentShipSize + 1);
-                col = std::rand() % BOARD_SIZE;
-            }
+	//placplayer 2
+	for (int i = 0; i < 4; i++) {
+		int currentShipSize = shipSizes[i];
+		bool placed = false;
+		int attempts = 0;
 
-            //Check if is able to place sentire boat 
-            bool canPlace = true;
-            for (int j = 0; j < currentShipSize; j++) {
-                int r = (dir == 1) ? row + j : row;
-                int c = (dir == 1) ? col : col + j;
+		while (!placed && attempts < 9999) {
+			attempts++;
 
-                if (boardPlayer2[r][c] != '~') {
-                    canPlace = false;
-                    break;
-                }
-            }
+			//Check for Horizontal or Vertical side first
+			int dir = std::rand() % 2;
+			int row, col = 0;
+			if (dir == 0) {
+				row = std::rand() % BOARD_SIZE;
+				col = std::rand() % (BOARD_SIZE - currentShipSize + 1);
+			}
+			else {
+				row = std::rand() % (BOARD_SIZE - currentShipSize + 1);
+				col = std::rand() % BOARD_SIZE;
+			}
 
-            //if all are free place it:
-            if (canPlace) {
-                for (int j = 0; j < currentShipSize; j++) {
-                    int r = (dir == 1) ? row + j : row;
-                    int c = (dir == 1) ? col : col + j;
-                    boardPlayer2[r][c] = shipSymbols[i];
-                }
-                placed = true;
-            }
-        }
-    }
+			//Check if is able to place sentire boat 
+			bool canPlace = true;
+			for (int j = 0; j < currentShipSize; j++) {
+				int r = (dir == 1) ? row + j : row;
+				int c = (dir == 1) ? col : col + j;
 
-    bool gameOver = false;
-    char boardDisplay[BOARD_SIZE] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-    
-    int currentPlayer = 0;
-    bool whenHitGiveAnotherTurn = false;
+				if (boardPlayer2[r][c] != WATER) {
+					canPlace = false;
+					break;
+				}
+			}
 
-    while (!gameOver) {
-        system("cls");
-        if (!whenHitGiveAnotherTurn) {
-            currentPlayer = (currentPlayer % 2) + 1;
-        }
-        else {
-            whenHitGiveAnotherTurn = true;
-        }
+			//if all are free place it:
+			if (canPlace) {
+				for (int j = 0; j < currentShipSize; j++) {
+					int r = (dir == 1) ? row + j : row;
+					int c = (dir == 1) ? col : col + j;
+					boardPlayer2[r][c] = shipSymbols[i];
+				}
+				placed = true;
+			}
+		}
+	}
 
-        std::cout << "Jugador 1: " << std::endl;
-        for (int i = 0; i < BOARD_SIZE; i++) {
-            std::cout << boardDisplay[i];
-            std::cout << ' ';
-        }
-        std::cout << std::endl;
-        for (int i = 0; i < BOARD_SIZE; ++i) {
-            for (int j = 0; j < BOARD_SIZE; ++j) {
-                std::cout << visibleBoardPlayer1[i][j] << ' ';
-            }
-            std::cout << boardDisplay[i];
-            std::cout << '\n';
-        }
-        std::cout << "----------------------\n";
-        std::cout << std::endl;
+	bool gameOver = false;
+	char boardDisplay[BOARD_SIZE] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 
-        std::cout << "Jugador 2: " << std::endl;
-        for (int i = 0; i < BOARD_SIZE; i++) {
-            std::cout << boardDisplay[i];
-            std::cout << ' ';
-        }
-        std::cout << std::endl;
-        for (int i = 0; i < BOARD_SIZE; ++i) {
-            for (int j = 0; j < BOARD_SIZE; ++j) {
-                std::cout << visibleBoardPlayer2[i][j] << ' ';
-            }
-            std::cout << boardDisplay[i];
-            std::cout << '\n';
-        }
-        std::cout << "----------------------\n";
+	int currentPlayer = 0;
+	bool whenHitGiveAnotherTurn = false;
 
-        int row, col;
-        std::cout << "Player " << currentPlayer << " enter row and column: ";
-        std::cin >> col >> row;
-        
-        if (currentPlayer == 2) {
-            if (boardPlayer1[row][col] == '3' || boardPlayer1[row][col] == '4' || boardPlayer1[row][col] == '5' || boardPlayer1[row][col] == '6') {
-                visibleBoardPlayer1[row][col] = 'X';
-                std::cout << "Hit!\n";
+	while (!gameOver) {
+		system("cls");
+		if (!whenHitGiveAnotherTurn) {
+			currentPlayer = (currentPlayer % 2) + 1;
+		}
+		else {
+			whenHitGiveAnotherTurn = false;
+		}
 
-                if (!whenHitGiveAnotherTurn) {
-                    whenHitGiveAnotherTurn = true;
-                }
-                else {
-                    whenHitGiveAnotherTurn = false;
-                }
-            }
-            else if (boardPlayer1[row][col] == '~') {
-                visibleBoardPlayer1[row][col] = 'O';
-                std::cout << "Miss!\n";
-                whenHitGiveAnotherTurn = false;
-            }
-        }
-        else if (currentPlayer == 1) {
-            if (boardPlayer2[row][col] == '3' || boardPlayer2[row][col] == '4' || boardPlayer2[row][col] == '5' || boardPlayer2[row][col] == '6') {
-                visibleBoardPlayer2[row][col] = 'X';
-                std::cout << "Hit!\n";
-            }
-            else if (boardPlayer2[row][col] == '~') {
-                visibleBoardPlayer2[row][col] = 'O';
-                std::cout << "Miss!\n";
-            }
-        }
+		//=== print board 1
+		std::cout << "Jugador 1: " << std::endl;
+		for (int i = 0; i < BOARD_SIZE; i++) {
+			std::cout << boardDisplay[i];
+			std::cout << ' ';
+		}
+		std::cout << std::endl;
+		for (int i = 0; i < BOARD_SIZE; ++i) {
+			for (int j = 0; j < BOARD_SIZE; ++j) {
+				std::cout << visibleBoardPlayer1[i][j] << ' ';
+			}
+			std::cout << boardDisplay[i];
+			std::cout << '\n';
+		}
+		std::cout << "----------------------\n";
+		std::cout << std::endl;
 
-        const int shipCount = 4;
-        bool destroyedShips[shipCount] = { false, false, false, false };
-        int remainingShipsPlayer1 = shipCount;
-        int remainingShipsPlayer2 = shipCount;
+		// print board 2
+		std::cout << "Jugador 2: " << std::endl;
+		for (int i = 0; i < BOARD_SIZE; i++) {
+			std::cout << boardDisplay[i];
+			std::cout << ' ';
+		}
+		std::cout << std::endl;
+		for (int i = 0; i < BOARD_SIZE; ++i) {
+			for (int j = 0; j < BOARD_SIZE; ++j) {
+				std::cout << visibleBoardPlayer2[i][j] << ' ';
+			}
+			std::cout << boardDisplay[i];
+			std::cout << '\n';
+		}
+		std::cout << "----------------------\n";
 
-        for (int s = 0; s < shipCount; s++) {
-            if (!destroyedShips[s]) {
-                bool exists = false;
-                for (int i = 0; i < BOARD_SIZE && !exists; i++) {
-                    for (int j = 0; j < BOARD_SIZE && !exists; j++) {
-                        if (boardPlayer1[i][j] == shipSymbols[s]) {
-                            exists = true;
-                        }
-                    }
-                }
-                if (!exists) {
-                    destroyedShips[s] = true;
-                    remainingShipsPlayer1--;
-                    std::cout << "Player " << currentPlayer << "destroyed ship " << shipSymbols[s] << "!\n";
-                }
-            }
-        }
+		//========== ask for input =============
+		int row, col;
+		std::cout << "Player " << currentPlayer << " enter row and column: ";
+		if (!(std::cin >> col >> row)) {
+			std::cerr << "Please enter two numbers.\n";
+			continue;
+		}
+		if (row < 0 || row >= BOARD_SIZE || col < 0 || col >= BOARD_SIZE) {
+			std::cerr << "Coordinates out of bounds. Try again.\n";
+			continue;
+		}
 
-        for (int s = 0; s < shipCount; s++) {
-            if (!destroyedShips[s]) {
-                bool exists = false;
-                for (int i = 0; i < BOARD_SIZE && !exists; i++) {
-                    for (int j = 0; j < BOARD_SIZE && !exists; j++) {
-                        if (boardPlayer2[i][j] == shipSymbols[s]) {
-                            exists = true;
-                        }
-                    }
-                }
-                if (!exists) {
-                    destroyedShips[s] = true;
-                    remainingShipsPlayer2--;
-                    std::cout << "Player " << currentPlayer << "destroyed ship " << shipSymbols[s] << "!\n";
-                }
-            }
-        }
+		//=========== check if tile that is selected is a ship and shoot???
+		std::cout << "You entered column " << col << " and row " << row << ".\n";
+		if (currentPlayer == 2) {
+			switch (boardPlayer1[row][col]) {
+				case '3':
+				case '4':
+				case '5':
+				case '6': {
+					visibleBoardPlayer1[row][col] = HIT_MARK;
+					whenHitGiveAnotherTurn = !whenHitGiveAnotherTurn;
+					break;
+				}
+				case WATER: {
+					visibleBoardPlayer1[row][col] = MISS_MARK;
+					whenHitGiveAnotherTurn = false;
+					break;
+				}
+				default: {
+					if (visibleBoardPlayer1[row][col] == HIT_MARK || visibleBoardPlayer1[row][col] == MISS_MARK) {
+						std::cout << "This spot was already choosen once" << std::endl;
+						break;
+					}
+					break;
+				}
+			}
+		}
 
-        if (remainingShipsPlayer1 < 0) {
-            gameOver = true;
-            std::cout << "Player 2 wins!" << std::endl;
-        } else if (remainingShipsPlayer2 < 0) {
-            gameOver = true;
-            std::cout << "Player 1 wins!" << std::endl;
-        }
+		else if (currentPlayer == 1) {
+			switch (boardPlayer2[row][col]) {
+				case '3':
+				case '4':
+				case '5':
+				case '6': {
+					visibleBoardPlayer2[row][col] = HIT_MARK;
+					whenHitGiveAnotherTurn = !whenHitGiveAnotherTurn;
+					break;
+				}
+				case WATER: {
+					visibleBoardPlayer2[row][col] = MISS_MARK;
+					whenHitGiveAnotherTurn = false;
+					break;
+				}
+				default: {
+					if (visibleBoardPlayer2[row][col] == HIT_MARK || visibleBoardPlayer2[row][col] == MISS_MARK) {
+						std::cout << "This spot was already choosen once" << std::endl;
+						break;
+					}
+					break;
+				}
+			}
+		}
 
-    }
+		// ======== check how many ships are alive or not
+		int remainingShipsPlayer1 = 0;
+		int remainingShipsPlayer2 = 0;
 
-    return 0;
+		for (int s = 0; s < SHIP_COUNT; ++s) {
+			bool alive1 = false, alive2 = false;
+
+			for (int i = 0; i < BOARD_SIZE && !(alive1 && alive2); ++i) {
+				for (int j = 0; j < BOARD_SIZE && !(alive1 && alive2); ++j) {
+					if (boardPlayer1[i][j] == shipSymbols[s]) alive1 = true;
+					if (boardPlayer2[i][j] == shipSymbols[s]) alive2 = true;
+				}
+			}
+
+			if (alive1) ++remainingShipsPlayer1;
+			if (alive2) ++remainingShipsPlayer2;
+			if (!alive1 && !alive2) {
+				std::cout << "Player " << currentPlayer
+					<< " destroyed ship " << shipSymbols[s] << "!\n";
+			}
+		}
+
+		//================ win if there are no remaaining ships
+		if (remainingShipsPlayer1 == 0) {
+			gameOver = true;
+			std::cout << "\n*** Player 2 wins! ***\n";
+			break;
+		}
+		if (remainingShipsPlayer2 == 0) {
+			gameOver = true;
+			std::cout << "\n*** Player 1 wins! ***\n";
+			break;
+		}
+
+	}
+	return 0;
 }
